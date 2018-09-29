80afb2cd350e67a2a265b28e538dd657936f36e8
Mohamed RACHID <EstevanTH@users.noreply.github.com>
2018-09-29 15:49:41 +0200

    HTTP: cleaner code for request handler
    The HTTP request handler is now a class that is not generated by a
    function.

--- OBDRelayHTTPServer.py
+++ OBDRelayHTTPServer.py
@@ -11,6 +11,76 @@ from time import time
 outputList = None
 outputListLock = None
 
+
+class OBDRelayHTTPRequestHandler( http.server.BaseHTTPRequestHandler ):
+	# Based on http.server.SimpleHTTPRequestHandler
+	server_version = "ELM327 OBD-II Relay"
+	
+	def do_GET( self ):
+		f = self.send_head()
+		if f:
+			try:
+				self.copyfile( f, self.wfile )
+			except ConnectionAbortedError:
+				pass
+			f.close()
+	
+	def do_HEAD( self ):
+		f = self.send_head( headersOnly=True )
+		if f:
+			f.close()
+	
+	def send_head( self, headersOnly=False ):
+		if self.path=="/vehicle.json":
+			global outputList
+			global outputListLock
+			expired = False
+			outputListCopy = None
+			outputListLock.acquire()
+			try:
+				outputListCopy = outputList.copy()
+			except:
+				pass
+			outputListLock.release()
+			if b"relaytime" not in outputListCopy:
+				expired = True # no data yet
+			elif self.server.thread.cacheExpire==0.0:
+				pass
+			elif time()>outputListCopy[b"relaytime"]+self.server.thread.cacheExpire:
+				expired = True
+			if not expired:
+				encoded = simpleDictionaryToJSON( outputListCopy )
+			if not expired:
+				response = HTTPStatus.OK
+				contentType = "application/json"
+			else:
+				encoded = b"No available up-to-date data"
+				response = HTTPStatus.GATEWAY_TIMEOUT
+				contentType = "text/plain"
+		else:
+			encoded = b"Not found"
+			response = HTTPStatus.NOT_FOUND
+			contentType = "text/plain"
+		
+		f = BytesIO()
+		f.write( encoded )
+		f.seek( 0 )
+		self.send_response( response )
+		self.send_header( "Content-type", contentType )
+		if headersOnly:
+			self.send_header( "Content-Length", "0" )
+		else:
+			self.send_header( "Content-Length", str( len( encoded ) ) )
+		self.send_header( "Access-Control-Allow-Origin", "*" )
+		self.end_headers()
+		return f
+	
+	def copyfile( self, source, outputfile ):
+		copyfileobj( source, outputfile )
+	
+	def log_request( self, code='-', size='-' ):
+		pass # no logging for successful requests
+
 class OBDRelayHTTPServerThread( threading.Thread ):
 	daemon = True # exit immediatly on program exit
 	
@@ -34,81 +104,8 @@ class OBDRelayHTTPServerThread( threading.Thread ):
 			"cacheExpire": self.cacheExpire,
 		}
 	
-	def makeRequestHandler( httpServer ):
-		class RequestHandler( http.server.BaseHTTPRequestHandler ):
-			# Based on http.server.SimpleHTTPRequestHandler
-			server_version = "ELM327 OBD-II Relay"
-			
-			def do_GET( self ):
-				f = self.send_head()
-				if f:
-					try:
-						self.copyfile( f, self.wfile )
-					except ConnectionAbortedError:
-						pass
-					f.close()
-			
-			def do_HEAD( self ):
-				f = self.send_head( headersOnly=True )
-				if f:
-					f.close()
-			
-			def send_head( self, headersOnly=False ):
-				if self.path=="/vehicle.json":
-					global outputList
-					global outputListLock
-					expired = False
-					outputListCopy = None
-					outputListLock.acquire()
-					try:
-						outputListCopy = outputList.copy()
-					except:
-						pass
-					outputListLock.release()
-					if b"relaytime" not in outputListCopy:
-						expired = True # no data yet
-					elif self.httpServer.cacheExpire==0.0:
-						pass
-					elif time()>outputListCopy[b"relaytime"]+self.httpServer.cacheExpire:
-						expired = True
-					if not expired:
-						encoded = simpleDictionaryToJSON( outputListCopy )
-					if not expired:
-						response = HTTPStatus.OK
-						contentType = "application/json"
-					else:
-						encoded = b"No available up-to-date data"
-						response = HTTPStatus.GATEWAY_TIMEOUT
-						contentType = "text/plain"
-				else:
-					encoded = b"Not found"
-					response = HTTPStatus.NOT_FOUND
-					contentType = "text/plain"
-				
-				f = BytesIO()
-				f.write( encoded )
-				f.seek( 0 )
-				self.send_response( response )
-				self.send_header( "Content-type", contentType )
-				if headersOnly:
-					self.send_header( "Content-Length", "0" )
-				else:
-					self.send_header( "Content-Length", str( len( encoded ) ) )
-				self.send_header( "Access-Control-Allow-Origin", "*" )
-				self.end_headers()
-				return f
-			
-			def copyfile( self, source, outputfile ):
-				copyfileobj( source, outputfile )
-			
-			def log_request( self, code='-', size='-' ):
-				pass # no logging for successful requests
-		
-		RequestHandler.httpServer = httpServer
-		
-		return RequestHandler
-	
 	def run( self ):
-		httpd = http.server.HTTPServer( (self.ipAddress, self.tcpPort), self.makeRequestHandler() )
+		httpd = http.server.HTTPServer( (self.ipAddress, self.tcpPort), OBDRelayHTTPRequestHandler )
+		httpd.thread = self
 		printT( "OBDRelayHTTPServerThread started:", self.ipAddress, self.tcpPort )
 		httpd.serve_forever()
