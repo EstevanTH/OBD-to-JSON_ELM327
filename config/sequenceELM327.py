38f37837e02103506e869d6958f74e9fd4a7f347
Yvon DUTAPIS <EstevanTH@users.noreply.github.com>
2019-08-16 13:52:09 +0200

    Update: Citroën Nemo 1.3 Multijet 75hp
    The sequence has been updated according to my own usage, in particular
    for boost measurements.

--- config/sequenceELM327.py
+++ config/sequenceELM327.py
@@ -18,6 +18,7 @@ obd.setPidResponseLength( 0x0D, 1, False ) # VEHICLE SPEED
 obd.setPidResponseLength( 0x0C, 2, False ) # ENGINE RPM
 obd.setPidResponseLength( 0x49, 1, False ) # ACCELERATOR PEDAL POSITION D
 obd.setPidResponseLength( 0x0B, 1, False ) # INTAKE MANIFOLD ABSOLUTE PRESSURE
+obd.setPidResponseLength( 0x2C, 1, False ) # COMMANDED EGR
 
 
 """ Declaration of CAN identifier requests ###
@@ -30,14 +31,15 @@ Instanciation synopsis:
 """
 
 reqBrake = obd.CanFrameRequest( 0x0810a000, 29, 3, expireTime_s=0.1 ) # BRAKE PEDAL POSITION
-reqAcceleratorRpm = obd.CanFrameRequest( 0x0618a001, 29, 8, expireTime_s=0.1 ) # ACCELERATOR PEDAL POSITION (EFFECTIVE) + ENGINE RPM
+reqAcceleratorRpmBoost = obd.CanFrameRequest( 0x0618a001, 29, 8, expireTime_s=0.1 ) # ACCELERATOR PEDAL POSITION (EFFECTIVE) + ENGINE RPM + BOOST
 reqSpeedAbs = obd.CanFrameRequest( 0x0210a006, 29, 6, expireTime_s=0.1 ) # VEHICLE SPEED + ABS
 
 
-""" Setup of functions that handle receiving OBD answers ###
+""" Setup of functions that handle receiving OBD answers / CAN packets ###
 
 Synopses:
 	obd.setPidResponseCallback( int pid, function receivedCallback )
+	obd.setPidResponseCallback( CanFrameRequest pid, function receivedCallback )
 	receivedCallback( int pid, bytes   data )
 	receivedCallback( int pid, int data )
 	obd.getLastResponseData( int pid )
@@ -47,7 +49,7 @@ Synopses:
 
 # BAROMETRIC PRESSURE
 def callback( pid, data ):
-	pass
+	obd.setCurrentOutputData( b"barometricPressure", data/100. ) # bars
 	# value only used elsewhere (to calculate the boost)
 obd.setPidResponseCallback( 0x33, callback )
 
@@ -58,7 +60,7 @@ obd.setPidResponseCallback( 0x05, callback )
 
 # FUEL LEVEL INPUT
 def callback( pid, data ):
-	obd.setCurrentOutputData( b"fuelLevel", data/255 ) # no unit
+	obd.setCurrentOutputData( b"fuelLevel", data/255. ) # no unit
 obd.setPidResponseCallback( 0x2F, callback )
 
 # DISTANCE TRAVELED WITH MIL ON
@@ -91,8 +93,13 @@ def callback( pid, pressionAdmi ):
 	# need both atmospheric pressure and intake manifold pressure to calculate boost
 	pressionAtmo = obd.getLastResponseData( 0x33 )
 	if pressionAtmo is not None:
-		obd.setCurrentOutputData( b"boostPressure", ( pressionAdmi-pressionAtmo )/100 ) # bars
+		obd.setCurrentOutputData( b"boostPressure", ( pressionAdmi-pressionAtmo )/100. ) # bars
 obd.setPidResponseCallback( 0x0B, callback )
+
+# COMMANDED EGR
+def callback( pid, data ):
+	obd.setCurrentOutputData( b"egrCommanded", data/255. ) # no unit
+obd.setPidResponseCallback( 0x2C, callback )
 
 # BRAKE PEDAL POSITION
 isAbsActive = False
@@ -108,26 +115,35 @@ def callback( identifier, data ):
 	obd.setCurrentOutputData( b"brakePosition", brakePosition ) # no unit
 obd.setPidResponseCallback( reqBrake, callback )
 
-# ACCELERATOR PEDAL POSITION (EFFECTIVE) + ENGINE RPM
+# ACCELERATOR PEDAL POSITION (EFFECTIVE) + ENGINE RPM + BOOST
 def callback( identifier, data ):
 	"""
 	Accelerator: Values are 0 to 255
 	Engine RPM: Value is in RPM
+	Boost: Values are in bars
 	"""
 	throttlePosition = data[7] # D7
 	throttlePosition /= 255.
 	engineRPM = data[2:4] # D2 to D3
 	engineRPM = int.from_bytes( engineRPM, "big" )
+	boost = data[1] # D1
+	boost -= 16
+	boost /= 128.
+	boostSmooth = data[4] # D4
+	boostSmooth -= 16
+	boostSmooth /= 128.
+	boostTarget = data[6] # D6
+	boostTarget -= 16
+	boostTarget /= 128.
 	obd.setCurrentOutputData( b"throttlePosition", throttlePosition ) # no unit
 	obd.setCurrentOutputData( b"engineRPM", engineRPM ) # RPM
-obd.setPidResponseCallback( reqAcceleratorRpm, callback )
+	obd.setCurrentOutputData( b"boostPressure", boost ) # boost
+	obd.setCurrentOutputData( b"boostSmooth", boostSmooth ) # boost (smoother)
+	obd.setCurrentOutputData( b"boostTarget", boostTarget ) # boost target
+obd.setPidResponseCallback( reqAcceleratorRpmBoost, callback )
 
 # VEHICLE SPEED + ABS
 def callback( identifier, data ):
-	"""
-	Accelerator: Values are 0 to 255
-	Engine RPM: Value is in RPM
-	"""
 	vehicleSpeed = data[4:6] # D4 to D5
 	vehicleSpeed = int.from_bytes( vehicleSpeed, "big" )
 	vehicleSpeed /= 128.
@@ -154,8 +170,9 @@ obd.resetSequence()
 def addPidListToSequence( subSequence ):
 	for pid in subSequence:
 		obd.addPidToSequence( pid )
-# subSequence1 = [0x0B, reqBrake, 0x49, 0x0D, 0x0C] # INTAKE PRESSURE / BRAKE / ACCELERATOR / SPEED / RPM
-subSequence1 = [0x0B, reqBrake, reqAcceleratorRpm, reqSpeedAbs] # INTAKE PRESSURE / BRAKE / ACCELERATOR + RPM / SPEED + ABS
+# subSequence1 = [0x0B, reqBrake, 0x49, 0x0D, 0x0C, 0x2C] # INTAKE PRESSURE / BRAKE / ACCELERATOR / SPEED / RPM / EGR
+# subSequence1 = [0x0B, reqBrake, reqAcceleratorRpmBoost, reqSpeedAbs, 0x2C] # INTAKE PRESSURE / BRAKE / ACCELERATOR + RPM / SPEED + ABS / EGR
+subSequence1 = [reqBrake, reqAcceleratorRpmBoost, reqSpeedAbs, 0x2C] # BRAKE / ACCELERATOR + RPM + BOOST / SPEED + ABS / EGR
 addPidListToSequence( subSequence1 )
 obd.addPidToSequence( 0x33 ) # BAROMETRIC PRESSURE
 addPidListToSequence( subSequence1 )
